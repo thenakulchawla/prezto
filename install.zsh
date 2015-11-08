9a5c77c69639642ad07f1b8d886b48c702e5223c
Jitesh Kamble <io@jite.sh>
2015-11-09 00:42:46 +0530

    Modify README; add install.zsh

---
+++ install.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+git clone --recursive https://github.com/hashknot/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    dest="${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    if [ -e "$dest" ]; then
+        mv "$dest" "$dest.bak"
+    fi
+    ln -s "$rcfile" "$dest"
+done
