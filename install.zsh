745016fa0f86bb66ef82e350a6e8ceeaffb47144
thenakulchawla <msg@nakul.net>
2020-05-30 14:04:26 -0700

    Update to thenakulchawla theme for further changes

--- install.zsh
+++ install.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-git clone --recursive https://github.com/hashknot/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/thenakulchawla/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
